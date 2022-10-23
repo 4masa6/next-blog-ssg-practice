299859e2881997170823ff4ea890d43c78cf56ce
mogata <70381096+4masa6@users.noreply.github.com>
2022-10-23 15:52:14 +0900

    カスタムappコンポーネントでレイアウトを管理

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
 import { Footer } from "../components/Footer";
 import { Header } from "../components/header";
 import { Hero } from "../components/Hero";
-import { Layout } from "../components/Layout/Layout";
 
 export default function Home() {
-  return (
-    <Layout>
-      <Hero></Hero>
-    </Layout>
-  );
+  return <Hero></Hero>;
 }
