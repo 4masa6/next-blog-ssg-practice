06769f47b01f99bcb58ae4654e8f1aab1eb24b24
mogata <70381096+4masa6@users.noreply.github.com>
2022-10-23 16:12:56 +0900

    Heroコンポーネントの画像表示フラグ追加

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,12 @@
 export const Hero = (props) => {
-  const { title, subtitle } = props;
+  const { title, subtitle, imageOn = false } = props;
 
   return (
     <>
       <div>
         <h1>{title}</h1>
         <p>{subtitle}</p>
+        {imageOn && <figure>[画像]</figure>}
       </div>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { Hero } from "../components/Hero";
 
 export default function Home() {
-  return <Hero title="SSG Blog" subtitle="ブログサイト" />;
+  return <Hero title="SSG Blog" subtitle="ブログサイト" imageOn />;
 }
