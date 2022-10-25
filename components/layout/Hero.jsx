c0a3be94f77c48df819e7d4f74eade5b226be92b
mogata <70381096+4masa6@users.noreply.github.com>
2022-10-25 18:38:57 +0900

    Heroコンポーネントのテキストデザイン

--- components/layout/Hero.jsx
+++ components/layout/Hero.jsx
@@ -1,13 +1,13 @@
-import style from '../../styles/hero.module.css'
+import styles from './styles/hero.module.css'
 
 export const Hero = (props) => {
   const { title, subtitle, imageOn = false } = props;
-  console.log(style);
+
   return (
     <>
-      <div className="style.text">
-        <h1 className="style.title">{title}</h1>
-        <p className="style.subtitle">{subtitle}</p>
+      <div className={styles.text}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.subtitle}>{subtitle}</p>
         {imageOn && <figure>[画像]</figure>}
       </div>
     </>
