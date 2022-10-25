0b6a68b0a804054a47526bbce470edd341674550
mogata <70381096+4masa6@users.noreply.github.com>
2022-10-25 19:57:03 +0900

    Containerコンポーネントで横幅を整える（Header・Footer）

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,13 +1,16 @@
 import { Logo } from '../atoms/Logo'
+import { Container } from './Container'
 import styles from './styles/footer.module.css'
 
 export const Footer = () => {
   return (
     <footer className={styles.wrapper}>
-      <div className={styles.flexContainer}>
-        <Logo />
-        [ソーシャルアイコン]
-      </div>
+      <Container>
+        <div className={styles.flexContainer}>
+          <Logo />
+          [ソーシャルアイコン]
+        </div>
+      </Container>
     </footer>
   )
 }
--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,14 +1,17 @@
 import { Logo } from "../atoms/Logo"
 import { Nav } from "../atoms/Nav"
+import { Container } from "./Container"
 import styles from './styles/header.module.css'
 
 export const Header = () => {
   return (
     <header>
-      <div className={styles.flexContainer}>
-        <Logo boxOn />
-        <Nav />
-      </div>
+      <Container large>
+        <div className={styles.flexContainer}>
+          <Logo boxOn />
+          <Nav />
+        </div>
+      </Container>
     </header>
   )
 }
