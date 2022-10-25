1c583ccabd25921aba54e4f2b2741ae678e2292a
mogata <70381096+4masa6@users.noreply.github.com>
2022-10-25 18:33:41 +0900

    strict mode off

--- components/layout/Hero.jsx
+++ components/layout/Hero.jsx
@@ -1,11 +1,13 @@
+import style from '../../styles/hero.module.css'
+
 export const Hero = (props) => {
   const { title, subtitle, imageOn = false } = props;
-
+  console.log(style);
   return (
     <>
-      <div>
-        <h1>{title}</h1>
-        <p>{subtitle}</p>
+      <div className="style.text">
+        <h1 className="style.title">{title}</h1>
+        <p className="style.subtitle">{subtitle}</p>
         {imageOn && <figure>[画像]</figure>}
       </div>
     </>
