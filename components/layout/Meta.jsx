3a673a8a84cdbe7d41c10b0827cc0f5373f56cf4
mogata <70381096+4masa6@users.noreply.github.com>
2022-10-29 17:42:50 +0900

    メタ情報にページのURL追加

--- components/layout/Meta.jsx
+++ components/layout/Meta.jsx
@@ -1,12 +1,18 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
+// サイトに関する情報
 import { siteMeta } from '../../lib/constants'
-
 const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } = siteMeta
 
 export const Meta = ({ pageTitle, pageDesc }) => {
+  // ページタイトル
   const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle ;
+  // ページの説明
   const desc = pageDesc ?? siteDesc;
+  // ページURL
+  const router = useRouter();
+  const url = `${siteUrl}${router.asPath}` // router.asPathでページのパス取得しページのURL生成。
 
   return (
     <Head>
@@ -15,6 +21,9 @@ export const Meta = ({ pageTitle, pageDesc }) => {
 
       <meta name="description" content={desc} />
       <meta property="og:description" content={desc} />
+
+      <link rel="canonical" href="url" />
+      <meta property="og:url" content={url} />
     </Head>
   )
 }
