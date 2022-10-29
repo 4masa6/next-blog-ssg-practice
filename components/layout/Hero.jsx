61fe586f6b201f6b5a2c66ab73bc8760d1876654
mogata <70381096+4masa6@users.noreply.github.com>
2022-10-29 16:01:44 +0900

    Heroコンポーネントに画像を指定

--- components/layout/Hero.jsx
+++ components/layout/Hero.jsx
@@ -1,4 +1,6 @@
 import styles from './styles/hero.module.css'
+import Image from 'next/image'
+import cube from '../../img/cube.jpg'
 
 export const Hero = (props) => {
   const { title, subtitle, imageOn = false } = props;
@@ -8,8 +10,18 @@ export const Hero = (props) => {
       <div className={styles.text}>
         <h1 className={styles.title}>{title}</h1>
         <p className={styles.subtitle}>{subtitle}</p>
-        {imageOn && <figure>[画像]</figure>}
       </div>
+      {imageOn && (
+        <figure className={styles.image}>
+          <Image
+            src={cube}
+            layout="responsive"
+            sizes="(min-width: 1152px) 576px, (min-width: 768px) 50vw, 100vw"
+            priority
+            placeholder="blur"
+          />
+        </figure>
+      )}
     </div>
   )
 }
