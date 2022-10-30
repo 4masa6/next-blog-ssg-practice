d7b26ed3b9902412f3a8e0ac1949c3c50ea2aff4
mogata <70381096+4masa6@users.noreply.github.com>
2022-10-30 16:26:07 +0900

    FontAwesomeを導入

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import { Layout } from "../components/layout/Layout";
 import "../styles/globals.css";
 
+// FontAwesomeの設定
+import "@fortawesome/fontawesome-svg-core/styles.css"; // FontAwesomeのCSSをグローバルスタイルとしてサイト全体に適用
+import { config } from "@fortawesome/fontawesome-svg-core"; // FontAwesomeのSVGコアが個別にCSSを適用するのを無効化
+config.autoAddCss = false;
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
