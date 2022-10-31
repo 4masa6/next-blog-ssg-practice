036bb5731eca21e2ee536ac2b03c5769344f88e9
mogata <70381096+4masa6@users.noreply.github.com>
2022-10-31 21:29:32 +0900

    関数で取得したデータを画面に表示する

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,20 +1,32 @@
-import { client } from '../../lib/api'
+import { getPostBySlug } from '../../lib/api'
+import { Container } from '../../components/layout/Container'
 
-export default function Schedule() {
-  return <h1>記事のタイトル</h1>
+export default function Schedule({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+}) {
+  return (
+    <Container>
+      <h1>{title}</h1>
+    </Container>
+  )
 }
 
 export async function getStaticProps() {
-  try {
-    const res = await client.get({
-      endpoint: 'blogs',
-    })
-    console.log(res)
-  } catch (err) {
-    console.log(err)
-  }
+  const slug = 'schedule'
+
+  const post = await getPostBySlug(slug)
 
   return {
-    props: {},
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch: post.eyecatch,
+      categories: post.categories,
+    },
   }
 }
