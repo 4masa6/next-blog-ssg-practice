a2d01f4991140f4ab21dd37d9f688d8a64e37a9a
mogata <70381096+4masa6@users.noreply.github.com>
2022-11-01 19:11:31 +0900

    外部サイトの画像をnext/imageで扱うためにドメインを指定

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-};
+  // アイキャッチ画像は外部サイトの画像のため、next/imageで扱えるようにドメインを指定する
+  images: {
+    domains: ['images.microcms-assets.io'],
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
