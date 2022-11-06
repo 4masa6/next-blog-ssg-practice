f3e0a95d9bec1c59ebf45f3b56f77ad1640818a5
mogata <70381096+4masa6@users.noreply.github.com>
2022-11-06 13:26:31 +0900

    トップページに最新の記事を表示する

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,39 @@
+import { getAllPosts } from '../lib/api'
+import { eyecatchLocal } from '../lib/constants'
+
 import { Meta } from '../components/layout/Meta'
 import { Container } from '../components/Layout/Container'
 import { Hero } from '../components/page-parts/Hero'
+import { Posts } from '../components/page-parts/Posts'
+import { Pagination } from '../components/page-parts/Pagination'
+import { getPlaiceholder } from 'plaiceholder'
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <Container>
       <Meta />
       <Hero title="SSG Blog" subtitle="ブログサイト" imageOn />
+
+      <Posts posts={posts} />
+      <Pagination nextUrl="blog/" nextText="もっと見る" />
     </Container>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
