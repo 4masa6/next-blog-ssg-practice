19fc5bb31aaf4daff82fe203f325f21986bb2c5e
mogata <70381096+4masa6@users.noreply.github.com>
2022-11-06 12:42:36 +0900

    記事一覧のアイキャッチ画像を表示

--- components/page-parts/Posts.jsx
+++ components/page-parts/Posts.jsx
@@ -1,13 +1,25 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import styles from './styles/posts.module.css'
 
 export const Posts = ({ posts }) => {
   return (
     <div className={styles.gridContainer}>
-      {posts.map(({ title, slug }) => (
+      {posts.map(({ title, slug, eyecatch }) => (
         <article className={styles.post} key={slug}>
           <Link href={`/blog/${slug}`}>
             <a>
+              <figure>
+                <Image
+                  src={eyecatch.url}
+                  alt=""
+                  layout="responsive"
+                  width={eyecatch.width}
+                  height={eyecatch.height}
+                  placeholder="blur"
+                  blurDataURL={eyecatch.blurDataURL}
+                />
+              </figure>
               <h2>{title}</h2>
             </a>
           </Link>
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,12 @@
 import { getAllPosts } from '../../lib/api'
+import { eyecatchLocal } from '../../lib/constants'
 
 import { Meta } from '../../components/layout/Meta'
 import { Container } from '../../components/Layout/Container'
 import { Hero } from '../../components/page-parts/Hero'
 import { Posts } from '../../components/page-parts/Posts'
+
+import { getPlaiceholder } from 'plaiceholder'
 
 export default function Blog({ posts }) {
   return (
@@ -20,6 +23,13 @@ export default function Blog({ posts }) {
 export async function getStaticProps() {
   const posts = await getAllPosts()
 
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
   return {
     props: {
       posts: posts,
