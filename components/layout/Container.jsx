666ce04465570a5afd49fb680c86f5c0023d9722
mogata <70381096+4masa6@users.noreply.github.com>
2022-11-13 12:13:28 +0900

    delete directory

--- components/layout/Container.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles/container.module.css'
-
-export const Container = ({ children, large = false }) => {
-  return (
-    <div className={large ? styles.large : styles.default}>
-      {children}
-    </div>
-  )
-}
