b1d5b2f06ca56b5be6031773339ceb2d1e7ee3d4
mogata <70381096+4masa6@users.noreply.github.com>
2022-11-13 11:11:47 +0900

    静的生成に対応のためnext/imageの画像処理APIを使うためのローダーをimgixに変更

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  // アイキャッチ画像は外部サイトの画像のため、next/imageで扱えるようにドメインを指定する
   images: {
+    // next/imgageがmicroCMSの imgixAPI を使うよう指定
+    loader: 'imgix',
+    path: '',
+    // アイキャッチ画像は外部サイトの画像のため、next/imageで扱えるようにドメインを指定する
     domains: ['images.microcms-assets.io'],
   },
 }
